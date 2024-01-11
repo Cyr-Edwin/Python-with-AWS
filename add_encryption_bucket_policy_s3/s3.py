78daa1c4a81980a9ee4663fc9d5fea02c8a90f96
pyEdwin <cyredwin1@gmail.com>
2024-01-11 09:13:41 -0500

    update s3.py file

--- add_encryption_bucket_policy_s3/s3.py
+++ add_encryption_bucket_policy_s3/s3.py
@@ -11,3 +11,16 @@ list_buckets = s3.list_buckets()
 buckets_name = []
 for bucket_name in list_buckets:
     buckets_name.append(bucket_name["Buckets"]["Name"])
+
+# find not encrypted bucket and encrypt them
+def encrypt_buckets():
+    # find encrypted bucket
+    for bucket_name in buckets_name:
+        try:
+            response = s3.get_bucket_encryption(Bucket= bucket_name) 
+            # information about the server-side encrytion
+            rules = response["ServerSideEncryptionConfiguration"]["Rules"]
+            # display the bucket name and rules used
+            print(f"Bucket Name:  {bucket_name} | Rules: {rules}\n")
+        except:
+            pass
