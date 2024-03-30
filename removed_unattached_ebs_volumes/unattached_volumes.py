f95e5cec976c75870d5cadda6a663131cfdaced1
pyEdwin <cyredwin1@gmail.com>
2024-03-30 12:45:43 -0400

    update unattached_volumes.py file

--- removed_unattached_ebs_volumes/unattached_volumes.py
+++ removed_unattached_ebs_volumes/unattached_volumes.py
@@ -13,3 +13,15 @@ for region in list_of_regions:
     # display Region
     print(f"Rigion ==> :  {region}")
 
+    # List unattached volumes (status : available)
+    volumes_unattached = ec2.volumes.filter(Filters={"Name":"status" , "Values":"available" })
+
+     # Delete volumes ids
+    for volume in volumes_unattached:
+        
+        vol = ec2.Volumes(volume.id)
+
+        print(f"{vol} is being deleting ..")
+
+        vol.delete()
+
